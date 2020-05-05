7f0bbe3a0385866a4bee9d41cf05bbf70b49797b
tortoise10 <tortoise10@hanmail.net>
2020-05-05 20:13:23 +0900

    개인정보 수정/ 목표대학 수정

--- PHPfile/update_targetUniversity.php
+++ PHPfile/update_targetUniversity.php
@@ -2,12 +2,12 @@
 $con = mysqli_connect("localhost", "nobles1030", "hero!0628", "nobles1030");
 
 $id = $_POST['id'];
-$password = $_POST['password'];
-$name = $_POST['name'];
-$mail = $_POST['mail'];
+$college1 = $_POST['college1'];
+$college2 = $_POST['college2'];
+$college3 = $_POST['college3'];
 
 
-$sql = "UPDATE PaceMakerMembers SET password = '".$password."', userName = '".$name."', userEmail = '".$mail."' WHERE id = '".$id."';";
+$sql = "UPDATE PaceMakerMembers SET targetUniversity1 = '".$college1."', targetUniversity2 = '".$college2."', targetUniversity3 = '".$college3."' WHERE id = '".$id."';";
 if(mysqli_query($con,$sql)){
   echo json_encode(success, JSON_UNESCAPED_UNICODE);
 } else {
