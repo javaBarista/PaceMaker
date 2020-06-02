796da41364b2cfd933123fc26e944d12f990ce4b
tortoise10 <tortoise10@hanmail.net>
2020-06-03 00:13:30 +0900

    시험분석

--- PHPfile/requestPreviousTest_analysis.php
+++ PHPfile/requestPreviousTest_analysis.php
@@ -8,21 +8,30 @@ $id=$_POST['id'];
 $db_sql = "SELECT score FROM PaceMaker_PreviousTests_analysis WHERE college = '".$college."' AND year = '".$year."' AND id = '".$id."';";
 $result = mysqli_query($conn, $db_sql);
 
-if(mysqli_num_rows($result) > 0){
+if(mysqli_query($con,$result)){
     $sql = "UPDATE PaceMaker_PreviousTests_analysis SET score = '".$score."' WHERE college = '".$college."' AND year = '".$year."' AND id = '".$id."';";
-    while($row = mysqli_fetch_assoc($sql)){
-      $array[] = $row;
-    }
+    mysqli_query($conn, $sql);
 }
 else{
     $sql = "INSERT INTO PaceMaker_PreviousTests_analysis(college, year, id, score) values('".$college."', '".$year."', '".$id."', '".$score."') ON DUPLICATE KEY UPDATE id = '".$id."';";
-    while($row = mysqli_fetch_assoc($sql)){
-      $array[] = $row;
-    }
+    mysqli_query($conn, $sql);
 }
 
-echo json_encode($array, JSON_UNESCAPED_UNICODE);
+$select_query = "SELECT COUNT(*) FROM `PaceMaker_PreviousTests_analysis` WHERE `college` = '".$college."' AND '".$year."';";
+$result_set = mysqli_query($conn, $select_query);
 
-mysql_free_result($data);
+for($i=0;$row1=mysqli_fetch_array($result_set);$i++){
+  $rank="SELECT (SELECT COUNT(*) + 1 FROM `PaceMaker_PreviousTests_analysis` WHERE score > a.score) AS rank
+  FROM `PaceMaker_PreviousTests_analysis` AS a
+  WHERE `college` = '".$college."' AND `year` = '".$year."' AND `id` = '".$id."'
+  ORDER BY a.score;";
+}
+
+if(mysqli_query($con,$rank)){
+  echo json_encode(success, JSON_UNESCAPED_UNICODE);
+} else {
+  echo json_encode(failure, JSON_UNESCAPED_UNICODE);
+}
+
 mysqli_close($conn);
 ?>
